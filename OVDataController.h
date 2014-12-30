12001f7653321a7b2ebfb386957ca4b75e92e3aa
Berend Schotanus <BS@BSchotanus.nl>
2014-12-30 09:45:51 +0100

    TimetableBrowser and rrrr_cocoa_wrapper are licensed under the Apache License, Version 2.0

--- OVDataController.h
+++ OVDataController.h
@@ -1,9 +1,21 @@
+//  Copyright (c) 2013-2014 First Flamingo Enterprise B.V.
 //
-//  OVDataContext.h
-//  OpenOVDemo
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+//  OVDataController.h
+//  rrrr_cocoa_wrapper
 //
 //  Created by Berend Schotanus on 08-11-13.
-//  Copyright (c) 2013 First Flamingo Enterprise B.V. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
